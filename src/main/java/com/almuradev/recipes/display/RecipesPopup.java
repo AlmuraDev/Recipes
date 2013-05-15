6cc85e0451e0e784df6c71a871dc20719dbe3026
Chris Sanders <zidane@outlook.com>
2013-05-14 22:19:05 -0500

    Compiles. Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/com/almuradev/recipes/display/RecipesPopup.java
+++ src/main/java/com/almuradev/recipes/display/RecipesPopup.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Recipes.
+ *
+ * © 2013 AlmuraDev <http://www.almuradev.com/>
+ * Recipes is licensed under the GNU General Public License.
+ *
+ * Recipes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Recipes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License. If not,
+ * see <http://www.gnu.org/licenses/> for the GNU General Public License.
+ */
 package com.almuradev.recipes.display;
 
 import java.util.ArrayList;
@@ -59,7 +78,6 @@ public class RecipesPopup extends GenericPopup {
 		comboBox.shiftXPos(-195).shiftYPos(-90);
 		comboBox.setHeight(20).setWidth(150);
 		comboBox.setSelection(0);
-		updateDropdown();
 
 		listWidget = new MyListWidget(this);
 		listWidget.setAnchor(WidgetAnchor.CENTER_CENTER);
